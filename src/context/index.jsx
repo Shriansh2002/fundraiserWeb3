30c50dc7be7966920ef46d95c9ac5a4e5cddd7db
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2023-01-06 16:53:30 +0530

    Web3 Contexting

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract(import.meta.env.VITE_CONTRACT_ADDRESS);
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
@@ -22,13 +22,13 @@ export const StateContextProvider = ({ children }) => {
         form.target,
         new Date(form.deadline).getTime(), // deadline,
         form.image
-      ])
+      ]);
 
-      console.log("contract call success", data)
+      console.log("contract call success", data);
     } catch (error) {
-      console.log("contract call failure", error)
+      console.log("contract call failure", error);
     }
-  }
+  };
 
   const getCampaigns = async () => {
     const campaigns = await contract.call('getCampaigns');
@@ -45,7 +45,7 @@ export const StateContextProvider = ({ children }) => {
     }));
 
     return parsedCampaings;
-  }
+  };
 
   const getUserCampaigns = async () => {
     const allCampaigns = await getCampaigns();
@@ -53,13 +53,13 @@ export const StateContextProvider = ({ children }) => {
     const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
 
     return filteredCampaigns;
-  }
+  };
 
   const donate = async (pId, amount) => {
-    const data = await contract.call('donateToCampaign', pId, { value: ethers.utils.parseEther(amount)});
+    const data = await contract.call('donateToCampaign', pId, { value: ethers.utils.parseEther(amount) });
 
     return data;
-  }
+  };
 
   const getDonations = async (pId) => {
     const donations = await contract.call('getDonators', pId);
@@ -67,20 +67,20 @@ export const StateContextProvider = ({ children }) => {
 
     const parsedDonations = [];
 
-    for(let i = 0; i < numberOfDonations; i++) {
+    for (let i = 0; i < numberOfDonations; i++) {
       parsedDonations.push({
         donator: donations[0][i],
         donation: ethers.utils.formatEther(donations[1][i].toString())
-      })
+      });
     }
 
     return parsedDonations;
-  }
+  };
 
 
   return (
     <StateContext.Provider
-      value={{ 
+      value={{
         address,
         contract,
         connect,
@@ -93,7 +93,7 @@ export const StateContextProvider = ({ children }) => {
     >
       {children}
     </StateContext.Provider>
-  )
-}
+  );
+};
 
 export const useStateContext = () => useContext(StateContext);
