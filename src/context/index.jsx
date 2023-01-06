bb31358b482739ae121c4f701f7651943d99871c
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2023-01-06 18:27:43 +0530

    final commit #VERSION1.0

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -62,7 +62,7 @@ export const StateContextProvider = ({ children }) => {
   };
 
   const getDonations = async (pId) => {
-    const donations = await contract.call('getDonators', pId);
+    const donations = await contract.call('gettDonators', pId);
     const numberOfDonations = donations[0].length;
 
     const parsedDonations = [];
